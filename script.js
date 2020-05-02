f7dd132cf9610ed2a3bf62392cdda28f690e0ef2
Christopher Marti <chrisjm093@gmail.com>
2020-05-02 08:40:54 -0700

    Local storage functionality working. Color of textinput cells cnow changes based on the time.

---
+++ script.js
@@ -0,0 +1,56 @@
+var currentDay = moment().format('dddd MMMM Do YYYY' );
+var currentTime = moment().format('HH');
+var hourBlock = $(this);
+var content = document.querySelector('.content');
+var saveBtns = $('.saveBtn');
+var storedTasks = JSON.parse(localStorage.getItem('tasks')) || {};
+
+//prints current date to the jumbotron
+$("#currentDay").append('<text>' + currentDay + '</text>' )
+//associates tasks from input fields to the hour that they a typed into
+function getTextInput( buttonHour ) {
+    return $('#task-' + buttonHour);
+}
+
+saveBtns.each(function() {
+   
+    var myButtonHour = $(this).data('hour');
+    var taskInput = getTextInput( myButtonHour );
+  
+    taskInput.val( storedTasks[ 'task' + myButtonHour]);
+   
+    //change color of textareas based on time
+    $(".content").each(function() {
+        if( myButtonHour == currentTime) {
+            $(this).addClass("present");
+        }
+        
+        else if (myButtonHour <= currentTime) {
+            $(this).addClass("past");
+        
+        } else {
+            $(this).addClass("future");
+        
+        }
+   
+});
+ 
+// Add new content to the tasks object when save button is clicked and saves to local storage as a JSON obj
+saveBtns.on('click', function() {
+     
+    var buttonHour = $(this).data('hour');
+    var taskInput = $('#task-' + buttonHour)
+    var taskValue = taskInput.val();
+       
+     
+    storedTasks[ 'task' + buttonHour ] = taskValue
+    
+    localStorage.setItem( 'tasks', JSON.stringify( storedTasks ));
+    
+       
+       
+     })
+   
+});
+     
+ getTextInput();
